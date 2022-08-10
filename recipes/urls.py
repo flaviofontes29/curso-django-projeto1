b2344f2f5d534022e1f795ba6e9fb435d40dae84
flaviofontes29 <flavio.carvalho.fontes@gmail.com>
2022-08-10 17:40:19 -0300

    configuração do urls recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path("", views.home), path("recipes/<int:id>/", views.recipe)]
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("recipes/<int:id>/", views.recipe, name="recipe"),
+]
