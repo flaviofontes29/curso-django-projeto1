273bd59ebd9b3f1f766c10e31a63d19c1835225d
flaviofontes29 <flavio.carvalho.fontes@gmail.com>
2022-08-14 19:19:55 -0300

    models

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("recipes/<int:id>/", views.recipe, name="recipe"),
+    path("recipes/<int:id>/", views.recipe, name="recipe"), 
 ]
