96a68af8dbbb864e9fc97f02f3cf9a538f60bc66
flaviofontes29 <flavio.carvalho.fontes@gmail.com>
2022-08-23 17:07:24 -0300

    Carregando dados com Model

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("recipes/<int:id>/", views.recipe, name="recipe"), 
+    path("recipes/category/<int:category_id>/", views.category, name="category"),
+    path("recipes/<int:id>/", views.recipe, name="recipe"),
 ]
