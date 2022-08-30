7d6f7d946f21e2218ba5a18f880e102af2b6f6ba
flaviofontes29 <flavio.carvalho.fontes@gmail.com>
2022-08-30 16:03:39 -0300

    test

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class RecipeURLsTest(TestCase):
+    def test_home_url_is_correct(self):
+        url = reverse("home")
+        self.assertEqual(url, "/")
+
+    def test_category_url_is_correct(self):
+        url = reverse("category", kwargs={"category_id": 1})
+        self.assertEqual(url, "/recipes/category/1/")
+
+    def test_recipe_url_is_correct(self):
+        url = reverse("recipe", kwargs={"id": 1})
+        self.assertEqual(url, "/recipes/1/")
